5cfe61b9cf85ab14e8ed007034280f8848876df1
DannyGP <dannygpineda@gmail.com>
2025-10-22 00:53:59 -0500

    final things deleted maybe

--- stockfish_engine.py
+++
@@ -1,6 +0,0 @@
-import chess.engine
-
-def analyze_position(board, depth=20):
-    with chess.engine.SimpleEngine.popen_uci("/usr/local/bin/stockfish") as engine:
-        result = engine.analyse(board, chess.engine.Limit(depth=depth))
-    return result
