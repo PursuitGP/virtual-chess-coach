07832e9b409e29919cdf2208dce54322f1358934
DannyGP <dannygpineda@gmail.com>
2025-10-22 00:56:06 -0500

    actually final I think lol

--- tests/test_parser.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from services.parser import parse_pgn
-
-def test_simple_pgn():
-    pgn = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6"
-    headers, moves = parse_pgn(pgn)
-    assert len(moves) == 6
-    assert moves[0]["san"] == "e4"
--- utils/winrate.py
+++
@@ -1,7 +0,0 @@
-def cp_to_winrate(cp: int) -> float:
-    """
-    Convert centipawn score into approximate winrate (0-100%).
-    This is a heuristic curve.
-    """
-    import math
-    return 50 + (50 * (2 / (1 + math.exp(-0.004 * cp)) - 1))
