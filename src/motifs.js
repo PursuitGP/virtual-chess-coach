4a991771be4f3d3f21aab20a6ecd1a23f00e4c99
CrastoDamasto <caporras202@gmail.com>
2025-11-25 01:28:06 -0600

    added hyperlinks to some motifs that lead to a sidebar with definetions and a crude miniboard for now

---
+++ src/motifs.js
@@ -0,0 +1,17 @@
+export const MOTIF_KEYWORDS = {
+  "fork": "motif-fork",
+  "pin": "motif-pin",
+  "skewer": "motif-skewer",
+  "discovered attack": "motif-discovered-attack",
+  "x-ray": "motif-xray",
+  "trapped piece": "motif-trapped",
+  "back-rank weakness": "motif-backrank",
+  "weak f7": "motif-weak-f7",
+  "center break": "motif-center-break",
+  "overextension": "motif-overextension",
+  "undefended piece": "motif-undefended",
+  "tempo loss": "motif-tempo",
+  "pawn break": "motif-pawn-break",
+  "isolated pawn": "motif-isolated-pawn",
+  "doubled pawns": "motif-doubled-pawns"
+};
